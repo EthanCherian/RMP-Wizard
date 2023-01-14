d92b04227593f9f04a97c7bf221585de45b2d7b9
Dien Chau <dienkchau@gmail.com>
2023-01-14 14:18:06 -0600

    changes

--- content-script/src/main.tsx
+++ content-script/src/main.tsx
@@ -3,30 +3,36 @@ import { createRoot } from 'react-dom/client';
 import './main.css'
 import App from './App'
 
-const body = document.querySelector('div[class*=PageWrapper]')!; // ! means we're sure this is never null, typescript
-const originalTab = document.querySelector('div[class*=TeacherRatingTabs][data-tabs]')!;
+// const body = document.querySelector('div[class*=PageWrapper]')!; // ! means we're sure this is never null, typescript
+// const originalTab = document.querySelector('div[class*=TeacherRatingTabs][data-tabs]')!;
 
-originalTab.remove();
+// originalTab.remove();
 
-const app = document.createElement('div')
+// const app = document.createElement('div')
 
-app.id = 'extension-new-tab'
+// app.id = 'extension-new-tab'
 
-// Make sure the element that you want to mount the app to has loaded. You can
-// also use `append` or insert the app using another method:
-// https://developer.mozilla.org/en-US/docs/Web/API/Element#methods
-//
-// Also control when the content script is injected from the manifest.json:
-// https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time
-if (body) {
-  // body.append(app);
-  body.insertBefore(app, body.lastChild);
-}
+// // Make sure the element that you want to mount the app to has loaded. You can
+// // also use `append` or insert the app using another method:
+// // https://developer.mozilla.org/en-US/docs/Web/API/Element#methods
+// //
+// // Also control when the content script is injected from the manifest.json:
+// // https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time
+// if (body) {
+//   // body.append(app);
+//   body.insertBefore(app, body.lastChild);
+// }
 
-const container = document.getElementById('extension-new-tab');
-const root = createRoot(container!);
+// const container = document.getElementById('extension-new-tab');
+// const root = createRoot(container!);
 
-root.render(
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
